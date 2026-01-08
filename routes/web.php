77f6ed30d7cf367fd9c555aedf5c798197899cb1
GibranMaulana <narbiganaluam@gmail.com>
2026-01-08 13:16:42 +0700

    (add)✨: setup route

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('sanctuary');
+})->name('sanctuary');
+
+Route::get('/collection', function() {
+   return view('collection');
+})->name('collection');
+
+Route::get('/philosophy', function() {
+   return view('philosophy');
+})->name('philosophy');
