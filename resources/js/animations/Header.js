dbfa9da702e6490c3969ea8f29b2e37b2720c330
GibranMaulana <narbiganaluam@gmail.com>
2026-01-14 01:33:16 +0700

    (add)✨: responsive and animation on header

--- resources/js/animations/Header.js
+++ resources/js/animations/Header.js
@@ -1,4 +1,5 @@
 import gsap from 'gsap'
+import { SplitText } from 'gsap/SplitText';
 
 export class Header{
    constructor() {
@@ -7,13 +8,17 @@ export class Header{
    }
 
    init() {
+      const headerSection = document.getElementById('header-section');
       this.ctx = gsap.context(() => {
-         const icon = document.getElementById('menu-icon');
-         const icontarget1 = icon.getElementById('header-icon-target-1');
-         const icontarget2 = icon.getElementById('header-icon-target-2');
-         const icontop = icon.getElementById('header-icon-top');
-         const iconmiddle = icon.getElementById('header-icon-middle');
-         const iconbottom = icon.getElementById('header-icon-bottom');
+
+         const icon = headerSection.querySelector('#menu-icon')
+         const icontarget1 = icon.querySelector('#header-icon-target-1');
+         const icontarget2 = icon.querySelector('#header-icon-target-2');
+         const icontop = icon.querySelector('#header-icon-top');
+         const iconmiddle = icon.querySelector('#header-icon-middle');
+         const iconbottom = icon.querySelector('#header-icon-bottom');
+
+         const headerLinks = headerSection.querySelectorAll('.header-links');
          
          let timeline = gsap.timeline({ paused: true, reversed: true })
 
@@ -29,6 +34,30 @@ export class Header{
          icon.addEventListener("click", () => {
             timeline.reversed() ? timeline.play() : timeline.reverse();
          })
-      })
+
+         //TODO: pake timeline nanti
+         gsap.from(headerLinks, {
+            duration: 2,
+            ease: 'power1.out',
+            y: -100
+         })
+
+         headerLinks.forEach(e => {
+            const splitter = SplitText.create(e, { type: "words, chars" })
+            const hoverTimeline = gsap.to(splitter.chars, {
+               paused: true,
+               duration: 0.4,
+               rotateY: -180,
+               stagger: { amount: 0.6}
+            })
+
+            e.addEventListener('mouseenter', () => {
+               hoverTimeline.play()
+            })
+            e.addEventListener('mouseleave', () => {
+               hoverTimeline.reverse()
+            })
+         })
+      }, headerSection)
    }
 }
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,16 +1,16 @@
-<header class="fixed inset-x-0 top-0 z-100 mix-blend-difference" id="header">
+<header class="fixed inset-x-0 top-0 z-100 mix-blend-difference" id="header-section">
    <div class="flex flex-row px-8 py-6 justify-between items-center">
-      <h2 class="font-headings text-white text-xl">
+      <h2 class="font-headings text-white text-xl header-links">
          BOTA<span class="inline-block scale-x-[-1]" >N</span>I<span class="inline-block scale-x-[-1]">C</span>A
       </h2>
 
-      <div class="hidden gap-10 text-white sm:flex">
-         <a href="{{ route('sanctuary')  }}" class="">SANCTUARY</a>
-         <a href="{{ route('collection')  }}" class="">COLLECTION</a>
-         <a href="{{ route('philosophy')  }}" class="">PHILOSOPHY</a>
+      <div class="hidden gap-10 text-white sm:flex overflow-y-hidden">
+         <a href="{{ route('sanctuary')  }}" class="header-links">SANCTUARY</a>
+         <a href="{{ route('collection')  }}" class="header-links">COLLECTION</a>
+         <a href="{{ route('philosophy')  }}" class="header-links">PHILOSOPHY</a>
       </div>
-      <div class="text-white">
-         <x-svgs.menu classname="text-white size-10"/>
+      <div class="text-white flex sm:hidden overflow-y-hidden">
+         <x-svgs.menu classname="text-white size-10 header-links"/>
       </div>
    </div>
 </header>
