5e9f18f6c31a70d157c3665eac6c2a1ab932e59d
GibranMaulana <narbiganaluam@gmail.com>
2026-02-07 00:01:50 +0700

    🔧 (fix): redesign product card

--- resources/views/components/sections/products-collection.blade.php
+++ resources/views/components/sections/products-collection.blade.php
@@ -8,18 +8,18 @@
       @foreach ($products as $p)
       <div class="relative max-w-[400px] w-xs mx-auto sm:flex flex-col p-4 m-2 col-span-1 rounded-lg product-card hover:cursor-pointer
                   snap-center shrink-0 min-w-[85%] ">
-         <div class="aspect-square sm:aspect-3/4 w-full overflow-hidden product-image-container rounded-md">
+         <div class="aspect-square sm:aspect-3/4 w-full overflow-hidden product-image-container rounded-t-md">
             <img src="{{ asset('storage/' . $p->images()->first()?->path) }}" alt="" 
             class="object-cover self-center w-full h-full product-image scale-125 sm:scale-none">
          </div>
-         <div class="my-3 product-content">
-            <div class="flex flex-row rounded-sm border border-accent text-accent text-sm w-fit items-center">
-               <p class="py-1 px-2">Available: </p>
+         <div class="relative product-content flex flex-col grow bg-[#FAFAF5] px-3 pt-4 pb-3 rounded-md -mt-2 z-20">
+            <div class="flex flex-row rounded-sm border border-accent text-accent text-sm w-fit items-center px-2">
+               <p class="py-1 pr-1">AVAILABLE: </p>
                @foreach ($p->variants()->get() as $variant)
-               <p class=" py-1 px-2">{{ $variant->size }}</p>
+               <p class=" py-1 pr-1">{{ $variant->size . ($loop->last ? "" : ",") }}</p>
                @endforeach
             </div>
-            <h3 class="text-3xl font-headings italic uppercase min-h-12">{{ $p->name }}</h3>
+            <h3 class="text-3xl font-headings italic uppercase min-h-12 mt-2">{{ $p->name }}</h3>
             <div class="flex flex-row justify-end gap-2 text-primary mt-auto">
                <p class="">{{ "$" . $p->variants->min('price') }}</p>
                @if ($p->variants->count() > 1)
@@ -27,7 +27,7 @@
                <p class="">{{ "$" . $p->variants->max('price') }}</p>
                @endif
             </div>
-            <div class="flex justify-end w-full mt-auto text-accent">
+            <div class="flex justify-end w-full text-accent">
                <a href="" class="font-headings text-xl w-fit buy-button">[ BUY NOW ]</a>
             </div>
          </div>
